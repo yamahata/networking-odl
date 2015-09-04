87f004eb6138e446dbec787ad51f49d9e71542e2
Isaku Yamahata <isaku.yamahata@intel.com>
2015-09-03 23:00:05 -0700

    db: prepare scripts for subproject db tables
    This patch prepares related files for introducing independent sub project
    tables.
    
    Change-Id: I4af931f5c3acf2e12081879669f9420426d074bb

---
+++ networking_odl/db/migration/alembic_migrations/versions/liberty/contract/383acb0d38a0_initial_odl_db.py
@@ -0,0 +1,34 @@
+# Copyright 2015 OpenStack Foundation
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+"""initial odl db
+
+Revision ID: 383acb0d38a0
+Revises: 11926bcfe72d
+Create Date: 2015-09-03 22:27:49.306394
+
+"""
+
+from neutron.db.migration import cli
+
+
+# revision identifiers, used by Alembic.
+revision = '383acb0d38a0'
+down_revision = None
+branch_labels = (cli.CONTRACT_BRANCH,)
+
+
+def upgrade():
+    pass
